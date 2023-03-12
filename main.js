165ec276cb56d3a64eaccb7ef2ecfcd7d8c76c55
Olehovych <olehbolonnuj@gmail.com>
2023-03-12 20:44:14 +0200

    first

---
+++ main.js
@@ -0,0 +1,7 @@
+AOS.init();
+const VIEW = document.getElementById("view");
+
+VIEW.addEventListener("click", function () {
+  const HIDDEN = document.getElementById("hidden");
+  HIDDEN.style.display = "flex";
+});
