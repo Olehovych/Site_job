5ece065e172f9d97096c11daf0f9bb5d009b2195
Olehovych <olehbolonnuj@gmail.com>
2023-03-16 01:48:20 +0200

    maybe_all

--- js/main.js
+++ js/main.js
@@ -3,5 +3,20 @@ const VIEW = document.getElementById("view");
 
 VIEW.addEventListener("click", function () {
   const HIDDEN = document.getElementById("hidden");
+
   HIDDEN.style.display = "flex";
+  HIDDEN.style.transition = "1s";
+});
+
+const QUESTION_BTNS = document.querySelectorAll("#faqs__btn");
+
+QUESTION_BTNS.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    const FAQS_SECTION = this.closest(".faqs__section_question");
+    const FAQS_TEXT = FAQS_SECTION.querySelector(".text");
+    FAQS_SECTION.style.height = "170px";
+    FAQS_SECTION.style.transition = "1s";
+    FAQS_TEXT.innerHTML =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+  });
 });
